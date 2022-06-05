06ab8b18c7859576136f13aee56713535bf8d397
mateusaffonso <augustomateus@poli.ufrj.br>
2022-06-05 03:35:37 -0300

    Updates

---
+++ precache-manifest.fb9b4e33b3177b016ef7ae5608ff1604.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "699094a88fe3c6b1afd6",
+    "url": "/mod3VNW-cronometro/static/css/main.08a95082.chunk.css"
+  },
+  {
+    "revision": "699094a88fe3c6b1afd6",
+    "url": "/mod3VNW-cronometro/static/js/main.699094a8.chunk.js"
+  },
+  {
+    "revision": "d11720e887a1448ed361",
+    "url": "/mod3VNW-cronometro/static/js/1.d11720e8.chunk.js"
+  },
+  {
+    "revision": "c63f8f9380c23bd7fd9b",
+    "url": "/mod3VNW-cronometro/static/js/2.c63f8f93.chunk.js"
+  },
+  {
+    "revision": "c4f17994286db19494c3",
+    "url": "/mod3VNW-cronometro/static/js/runtime~main.c4f17994.js"
+  },
+  {
+    "revision": "96d78cb0d34db7d5db9c5f8dd614bf48",
+    "url": "/mod3VNW-cronometro/static/media/corrida-nike.96d78cb0.gif"
+  },
+  {
+    "revision": "84d96ae4f5ec19a5b16d4413d54a41eb",
+    "url": "/mod3VNW-cronometro/index.html"
+  }
+];
